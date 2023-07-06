ca0037d20bf3d01b724871a6a7d19d9e0d8e2928
skmdab <57156958+skmdab@users.noreply.github.com>
2023-07-06 09:56:58 +0530

    Update aws_create.sh

--- aws_create.sh
+++ aws_create.sh
@@ -55,4 +55,11 @@ PUBLICIP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reser
 
 LINE="[$INSTANCENAME]\n\n$PUBLICIP ansible_user=ec2-user ansible_ssh_private_key_file=/home/ansible/filinta.pem"
 
-echo -e "$LINE" > /etc/ansible/hosts
+PATH="/var/lib/jenkins/workspace/sonarqube"
+
+if [ "$(echo "$PWD")" = "$PATH" ]; then
+  echo "$LINE" > /etc/ansible/hosts
+else
+  echo "$LINE" > hosts
+fi
+
